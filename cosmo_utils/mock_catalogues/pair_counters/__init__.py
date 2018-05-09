ebfc11c379aa1c13c237d603ee6ddb58f2cff1b5
Victor Calderon <victor.calderon90@gmail.com>
2018-05-09 13:19:35 -0500

    Added `pair_counter_rp` cython function to package

---
+++ cosmo_utils/mock_catalogues/pair_counters/__init__.py
@@ -0,0 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+# This sub-module is destined for common non-package specific utility
+# functions.
+
+from __future__ import absolute_import
+
+from cosmo_utils.mock_catalogues.pair_counters import pair_counter_rp
