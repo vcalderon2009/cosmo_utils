c8e663cb3f4b07b7c83a3d7ff3ba30f6964af6ff
Victor Calderon <victor.calderon90@gmail.com>
2018-05-11 06:07:42 -0500

    Added 'stats' and reorder modules in '__init__.py' files

--- cosmo_utils/utils/__init__.py
+++ cosmo_utils/utils/__init__.py
@@ -8,8 +8,6 @@ from __future__ import absolute_import
 from cosmo_utils.utils import file_utils
 from cosmo_utils.utils import work_paths
 from cosmo_utils.utils import file_readers
+from cosmo_utils.utils import stats_funcs
 from cosmo_utils.utils import geometry
 from cosmo_utils.utils import web_utils
-
-# from . import stats_funcs
-# from . import file_readers
