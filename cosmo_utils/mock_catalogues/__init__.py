6c6272d96087253cb02bd8c61e4ad0043a4f077e
Victor Calderon <victor.calderon90@gmail.com>
2018-05-15 11:22:50 -0500

    Updated __init__.py file

--- cosmo_utils/mock_catalogues/__init__.py
+++ cosmo_utils/mock_catalogues/__init__.py
@@ -8,5 +8,6 @@ from __future__ import absolute_import
 from cosmo_utils.mock_catalogues import abundance_matching
 from cosmo_utils.mock_catalogues import catls_utils
 from cosmo_utils.mock_catalogues import mags_calculations
+from cosmo_utils.mock_catalogues import shmr_funcs
 from cosmo_utils.mock_catalogues import spherematch
 from cosmo_utils.mock_catalogues import pair_counters
