de878df3d3be1b0494bf443a59bbf8eb76c6267b
Victor Calderon <victor.calderon90@gmail.com>
2019-03-06 02:55:43 -0600

    Added relations for `behroozi2010` and `moster 2010` models

--- cosmo_utils/mock_catalogues/shmr_funcs.py
+++ cosmo_utils/mock_catalogues/shmr_funcs.py
@@ -3,22 +3,483 @@
 
 # Victor Calderon
 # Created      : 2018-05-14
-# Last Modified: 2019-02-17
+# Last Modified: 2019-03-06
 from __future__ import absolute_import, division, print_function
 __author__     = ['Victor Calderon']
 __copyright__  = ["Copyright 2018 Victor Calderon"]
 __email__      = ['victor.calderon@vanderbilt.edu']
 __maintainer__ = ['Victor Calderon']
-__all__        = [  "Behroozi_relation",
-                    "Moster2010_relation",
-                    "Moster_example"]
+__all__        = [  'Behroozi2010Relation',
+                    'Moster2010Relation']
 
 ## Import modules
 import numpy as np
+import pandas as pd
 from   cosmo_utils.utils             import file_utils as fd
 from   cosmo_utils.custom_exceptions import LSSUtils_Error
 
-## Functions
+## Functions and Classes
+
+# Behroozi Relation class
+class Behroozi2010Relation(object):
+    """
+    Class used to define the stellar-halo mass relation of
+    central galaxies as a function of halo mass.
+    """
+    def __init__(self, **kwargs):
+        r"""
+        Parameters
+        -----------
+        input_h : {`int`, `float`} optional
+            Value of the Hubble constant in limits of between :math:`[0, 1]`.
+            This variable acts as the input `h` of variables, and it is set
+            to `1` by default.
+
+        out_h : {`int`, `float`} optional
+            Value of the Hubble constant in limits of between :math:`[0, 1]`.
+            This variable acts as the output `h` of variables, and it is set
+            to `1` by default.
+
+        Examples
+        ------------
+        >>> from cosmo_utils.mock_catalogues.shmr_funcs import BehrooziRelation
+        """
+        # Assigning variables
+        self.input_h = kwargs.get('input_h', 1)
+        self.out_h   = kwargs.get('out_h', 1)
+        # Checking input parameters
+        self._check_input_parameters()
+
+    # Checking input parameters to make sure they are `expected`
+    def _check_input_parameters(self):
+        r"""
+        Checks whether or not the input parameters are what is expected or not.
+        """
+        # Checking input types
+        h_type_arr = (float, int)
+        # `input_h` - Type
+        if not (isinstance(self.input_h, h_type_arr)):
+            msg = '`input_h` {0} is not a valid input type: {1}'
+            msg = msg.format(type(self.input_h), h_type_arr)
+            raise TypeError(msg)
+        # Checking input values
+        if not ((self.input_h > 0) and (self.input_h <= 1)):
+            msg = '`input_h` ({0}) is not within the proper range [0, 1]!'
+            msg = msg.format(self.input_h)
+            raise ValueError(msg)
+        # `out_h` - Type
+        if not (isinstance(self.out_h, h_type_arr)):
+            msg = '`out_h` {0} is not a valid input type: {1}'
+            msg = msg.format(type(self.out_h), h_type_arr)
+            raise TypeError(msg)
+        # Checking input values
+        if not ((self.out_h > 0) and (self.out_h <= 1)):
+            msg = '`out_h` ({0}) is not within the proper range [0, 1]!'
+            msg = msg.format(self.out_h)
+            raise ValueError(msg)
+
+    # Default dictionary to return to the user
+    def _return_default_dict(self):
+        r"""
+        Creates the directory used for the end-user with input variables of
+        the model.
+
+        Returns
+        -----------
+        param_dict : `dict`
+            Dictionary containing default model parameter values of
+            Behroozi et al. (2010) paper, plus any additional input
+            parameters to the model.
+        """
+        param_dict            = self._retrieve_model_default_dict()
+        param_dict['input_h'] = self.input_h
+        param_dict['out_h']   = self.out_h
+
+        return param_dict
+
+    # Default dictionary with input parameters
+    def _retrieve_model_default_dict(self):
+        r"""
+        Dictionary of the default values of all model parameters set to the
+        column 2 values in Table 2 of Behroozi et al. (2010) publication.
+
+        Returns
+        ----------
+        d : `dict`
+            Dictionary containing default model parameter values of
+            Behroozi et al. (2010) paper.
+
+        Notes
+        ---------
+        All calculations are done internally using the same h=0.7 units as
+        in Behroozi et al. (2010), ['arXiv:1001.0015'] so the parameter values
+        here are the same as in Table 2, even though the `mean_log_halo_mass`
+        and `mean_stellar_mass` methods use accept and return arguments
+        in ``h = 1`` units.
+        """
+        # Main dictionary
+        d = ({'smhm_m0_0': 10.72,
+            'smhm_m0_a': 0.59,
+            'smhm_m1_0': 12.35,
+            'smhm_m1_a': 0.3,
+            'smhm_beta_0': 0.43,
+            'smhm_beta_a': 0.18,
+            'smhm_delta_0': 0.56,
+            'smhm_delta_a': 0.18,
+            'smhm_gamma_0': 1.54,
+            'smhm_gamma_a': 2.52})
+
+        return d
+
+    # Mean relation of the model to compute the mean halo mass of galaxies.
+    def mean_log_halo_mass(self, log_mstar, z=0):
+        r"""
+        Returns the halo mass of a central galaxy as a function of its stellar
+        mass.
+
+        Parameters
+        -----------
+        log_mstar : {`float`, `numpy.ndarray`, `list`}
+            Logarithmic 10-base stellar mass array of the galaxies.
+
+        z : {`float`, `int`}, optional
+            Redshift of the halo hosting the galaxy. If passing an array,
+            it must be the same length as the input `log_mstar`.
+
+        Returns
+        --------
+        mass_dict : `dict`
+            Dictionary with the logarithmic 10-base stellar mass and halo
+            mass arrays in the same units as `self.out_h`.
+        """
+        # file_msg = fd.Program_Msg(__file__)
+        file_msg = '>>>  '
+        little_h = 0.7
+        ## Checking input parameters
+        # `log_mstar`
+        log_mstar_valid_types = (int, float, np.ndarray, list)
+        if not (isinstance(log_mstar, log_mstar_valid_types)):
+            msg = '{0} `log_mstar` ({1}) is not a valid input type: {2}!'
+            msg = msg.format(file_msg, type(log_mstar), log_mstar_valid_types)
+            raise TypeError(msg)
+        # `z` - Type
+        z_valid_types = (int, float)
+        if not (isinstance(z, z_valid_types)):
+            msg = '{0} `z` ({1}) is not a valid input type: {2}!'
+            msg = msg.format(file_msg, type(z), z_valid_types)
+            raise TypeError(msg)
+        # `z` - Value
+        if not (z >= 0):
+            msg = '{0} `z` ({1}) must be larger than `0`!'
+            msg = msg.format(file_msg, z)
+            raise ValueError(msg)
+        #
+        # Retrieving parameter dictionary
+        param_dict = self._retrieve_model_default_dict()
+        ## Checking units of `log_mstar` and converting to ``h = 0.7`` if
+        ## necessary.
+        # Converting stellar mass from units of `self.input_h` to `h == 1`
+        # and to `h = 0.7`
+        mstar_h1 = (10**log_mstar) * (self.input_h**2)
+        # Converting to `h=0.7` units
+        mstar    = mstar_h1 / (little_h**2)
+        # Scale factor
+        a = 1./(1. + z)
+        # Behroozi function
+        logm0 = param_dict['smhm_m0_0'] + (param_dict['smhm_m0_a'] * (a - 1))
+        m0    = 10.**logm0
+        logm1 = param_dict['smhm_m1_0'   ] + (param_dict['smhm_m1_a'] * (a - 1))
+        beta  = param_dict['smhm_beta_0' ] + param_dict['smhm_beta_a' ]*(a - 1)
+        delta = param_dict['smhm_delta_0'] + param_dict['smhm_delta_a']*(a - 1)
+        gamma = param_dict['smhm_gamma_0'] + param_dict['smhm_gamma_a']*(a - 1)
+        #
+        stellar_mass_by_m0 = mstar / m0
+        term3_numerator = stellar_mass_by_m0**delta
+        term3_denominator = 1. + (stellar_mass_by_m0)**(-gamma)
+        #
+        # In units of `h = 0.7`
+        log_halo_mass  = logm1 + (beta * np.log10(stellar_mass_by_m0))
+        log_halo_mass += (term3_numerator / term3_denominator) - 0.5
+        halo_mass      = 10.**log_halo_mass
+        # Converting to desired units
+        log_mstar_key = np.log10(mstar * (little_h**2) / (self.out_h**2))
+        log_mhalo_key = np.log10(halo_mass * (little_h) / (self.out_h))
+        # Saving to dictionary
+        dict_names = ['log_mhalo', 'log_mstar']
+        dict_vars  = [log_mhalo_key, log_mstar_key]
+        mass_dict  = dict(zip(dict_names, dict_vars))
+
+        return mass_dict
+
+    def compute_example(self, log_mstar_min=8, log_mstar_max=12, mstep=500,
+        return_pd=True):
+        """
+        Computes the ``Behroozi et al. (2010)`` relation for central galaxies.
+
+        Parameters
+        ------------
+        log_mstar_min : {`float`, `int`}, optional
+            Logarithmic 10-base minimum stellar mass. This variable is set to
+            `10` by default.
+
+        log_mstar_max : {`float`, `int`}, optional
+            Logarithmic 10-base maximum stellar mass. This variable is set to
+            `15` by default.
+
+        mstep : {`float`, `int`} optional
+            Number of elements in the output arrays of halo masses and
+            stellar masses. This variable is set to `100` by default.
+
+        return_pd : `bool`, optional
+            If `True`, the function returns a `pandas.DataFrame` with the
+            logarithmic 10-base halo masses and stellar masses of central
+            galaxies in the units of `self.out_h`. This variable is set
+            to `True` by default.
+
+        Returns
+        ---------
+        mass_obj : {`dict`, `pandas.DataFrame`}
+            Dictionary containing arrays or floats of the logarithmic
+            10-base halo mass and stellar masses of central galaxies in
+            units of `self.out_h`. If ``return_pd == True``, this function
+            returns a `pandas.DataFrame` instead.
+        """
+        # Constants
+        redshift = 0.
+        # Defining logarithmic 10-base stellar mass array
+        log_stellar_arr = np.linspace(log_mstar_min, log_mstar_max, mstep)
+        # Computing dictionary of the Behroozi relation
+        mass_dict = self.mean_log_halo_mass(log_stellar_arr, z=redshift)
+        # Returning DataFrame if needed
+        if return_pd:
+            mass_obj = pd.DataFrame(mass_dict)
+        else:
+            mass_obj = mass_dict
+
+        return mass_obj
+
+class Moster2010Relation(object):
+    """
+    Class used to define the stellar-halo mass relation of
+    central galaxies as a function of halo mass.
+    """
+    def __init__(self, **kwargs):
+        r"""
+        Parameters
+        -----------
+        input_h : {`int`, `float`} optional
+            Value of the Hubble constant in limits of between :math:`[0, 1]`.
+            This variable acts as the input `h` of variables, and it is set
+            to `1` by default.
+
+        out_h : {`int`, `float`} optional
+            Value of the Hubble constant in limits of between :math:`[0, 1]`.
+            This variable acts as the output `h` of variables, and it is set
+            to `1` by default.
+
+        Examples
+        ------------
+        >>> from cosmo_utils.mock_catalogues.shmr_funcs import BehrooziRelation
+        """
+        # Assigning variables
+        self.input_h = kwargs.get('input_h', 1)
+        self.out_h   = kwargs.get('out_h', 1)
+        # Checking input parameters
+        self._check_input_parameters()
+
+    # Checking input parameters to make sure they are `expected`
+    def _check_input_parameters(self):
+        r"""
+        Checks whether or not the input parameters are what is expected or not.
+        """
+        # Checking input types
+        h_type_arr = (float, int)
+        # `input_h` - Type
+        if not (isinstance(self.input_h, h_type_arr)):
+            msg = '`input_h` {0} is not a valid input type: {1}'
+            msg = msg.format(type(self.input_h), h_type_arr)
+            raise TypeError(msg)
+        # Checking input values
+        if not ((self.input_h > 0) and (self.input_h <= 1)):
+            msg = '`input_h` ({0}) is not within the proper range [0, 1]!'
+            msg = msg.format(self.input_h)
+            raise ValueError(msg)
+        # `out_h` - Type
+        if not (isinstance(self.out_h, h_type_arr)):
+            msg = '`out_h` {0} is not a valid input type: {1}'
+            msg = msg.format(type(self.out_h), h_type_arr)
+            raise TypeError(msg)
+        # Checking input values
+        if not ((self.out_h > 0) and (self.out_h <= 1)):
+            msg = '`out_h` ({0}) is not within the proper range [0, 1]!'
+            msg = msg.format(self.out_h)
+            raise ValueError(msg)
+
+    # Default dictionary to return to the user
+    def _return_default_dict(self):
+        r"""
+        Creates the directory used for the end-user with input variables of
+        the model.
+
+        Returns
+        -----------
+        param_dict : `dict`
+            Dictionary containing default model parameter values of
+            Moster et al. (2010) paper, plus any additional input
+            parameters to the model.
+        """
+        param_dict            = self._retrieve_model_default_dict()
+        param_dict['input_h'] = self.input_h
+        param_dict['out_h']   = self.out_h
+
+        return param_dict
+
+    # Default dictionary with input parameters
+    def _retrieve_model_default_dict(self):
+        r"""
+        Dictionary of the default values of all model parameters set to the
+        column 2 values in Table 2 of Moster et al. (2013) publication.
+
+        Returns
+        ----------
+        d : `dict`
+            Dictionary containing default model parameter values of
+            Moster et al. (2010) paper.
+
+        Notes
+        ---------
+        All calculations are done internally using the same h=0.7 units as
+        in Moster et al. (2010), ['arXiv:0903.4682'] so the parameter values
+        here are the same as in Table 2, even though the `mean_log_halo_mass`
+        and `mean_stellar_mass` methods use accept and return arguments
+        in ``h = 1`` units.
+        """
+        # Main dictionary
+        d = ({'smhm_m1':11.899,
+            'smhm_m_M_0':0.02817,
+            'smhm_beta':1.068,
+            'smhm_gamma':0.611})
+
+        return d
+
+    # Mean relation of the model to compute the mean halo mass of galaxies.
+    def mean_log_stellar_mass(self, log_halo_mass, z=0):
+        r"""
+        Returns the halo mass of a central galaxy as a function of its halo
+        mass.
+
+        Parameters
+        -----------
+        log_halo_mass : {`float`, `numpy.ndarray`, `list`}
+            Logarithmic 10-base halo mass array of the galaxies.
+
+        z : {`float`, `int`}, optional
+            Redshift of the halo hosting the galaxy. If passing an array,
+            it must be the same length as the input `log_mstar`.
+
+        Returns
+        --------
+        mass_dict : `dict`
+            Dictionary with the logarithmic 10-base stellar mass and halo
+            mass arrays in the same units as `self.out_h`.
+        """
+        # file_msg = fd.Program_Msg(__file__)
+        file_msg = '>>>  '
+        little_h = 0.72
+        ## Checking input parameters
+        # `log_halo_mass`
+        log_halo_mass_valid_types = (int, float, np.ndarray, list)
+        if not (isinstance(log_halo_mass, log_halo_mass_valid_types)):
+            msg = '{0} `log_halo_mass` ({1}) is not a valid input type: {2}!'
+            msg = msg.format(file_msg, type(log_halo_mass),
+                log_halo_mass_valid_types)
+            raise TypeError(msg)
+        # `z` - Type
+        z_valid_types = (int, float)
+        if not (isinstance(z, z_valid_types)):
+            msg = '{0} `z` ({1}) is not a valid input type: {2}!'
+            msg = msg.format(file_msg, type(z), z_valid_types)
+            raise TypeError(msg)
+        # `z` - Value
+        if not (z >= 0):
+            msg = '{0} `z` ({1}) must be larger than `0`!'
+            msg = msg.format(file_msg, z)
+            raise ValueError(msg)
+        #
+        # Retrieving parameter dictionary
+        param_dict = self._retrieve_model_default_dict()
+        ## Checking units of `log_halo_mass` and converting to ``h = 0.7`` if
+        ## necessary.
+        # Converting halo mass from units of `self.input_h` to `h == 1`
+        # and to `h = 0.7`
+        halo_mass_h1 = (10**log_halo_mass) * (self.input_h)
+        # Converting to `h=0.7` units
+        halo_mass    = halo_mass_h1 / (little_h)
+        # Scale factor
+        a = 1./(1. + z)
+        # Moster 2010 function
+        halo_mass_by_m1 = halo_mass / (10.**param_dict['smhm_m1'])
+        m_m1_beta       = halo_mass_by_m1**(-param_dict['smhm_beta'])
+        m_m1_gamma      = halo_mass_by_m1**(param_dict['smhm_gamma'])
+        inverse_term    = (m_m1_beta + m_m1_gamma)**(-1)
+        mass_over_M     = 2. * param_dict['smhm_m_M_0'] * inverse_term
+        mstar           = halo_mass * mass_over_M
+        # Converting to desired units
+        log_mstar_key = np.log10(mstar * (little_h**2) / (self.out_h**2))
+        log_mhalo_key = np.log10(halo_mass * (little_h) / (self.out_h))
+        # Saving to dictionary
+        dict_names = ['log_mhalo', 'log_mstar']
+        dict_vars  = [log_mhalo_key, log_mstar_key]
+        mass_dict  = dict(zip(dict_names, dict_vars))
+
+        return mass_dict
+
+    def compute_example(self, log_mhalo_min=10, log_halo_max=15, mstep=500,
+        return_pd=True):
+        """
+        Computes the ``Moster et al. (2010)`` relation for central galaxies.
+
+        Parameters
+        ------------
+        log_mhalo_min : {`float`, `int`}, optional
+            Logarithmic 10-base minimum halo mass. This variable is set to
+            `10` by default.
+
+        log_halo_max : {`float`, `int`}, optional
+            Logarithmic 10-base maximum halo mass. This variable is set to
+            `15` by default.
+
+        mstep : {`float`, `int`} optional
+            Number of elements in the output arrays of halo masses and
+            halo masses. This variable is set to `100` by default.
+
+        return_pd : `bool`, optional
+            If `True`, the function returns a `pandas.DataFrame` with the
+            logarithmic 10-base halo masses and stellar masses of central
+            galaxies in the units of `self.out_h`. This variable is set
+            to `True` by default.
+
+        Returns
+        ---------
+        mass_obj : {`dict`, `pandas.DataFrame`}
+            Dictionary containing arrays or floats of the logarithmic
+            10-base halo mass and stellar masses of central galaxies in
+            units of `self.out_h`. If ``return_pd == True``, this function
+            returns a `pandas.DataFrame` instead.
+        """
+        # Constants
+        redshift = 0.
+        # Defining logarithmic 10-base stellar mass array
+        log_mhalo_arr = np.linspace(log_mhalo_min, log_halo_max, mstep)
+        # Computing dictionary of the Behroozi relation
+        mass_dict = self.mean_log_stellar_mass(log_mhalo_arr, z=redshift)
+        # Returning DataFrame if needed
+        if return_pd:
+            mass_obj = pd.DataFrame(mass_dict)
+        else:
+            mass_obj = mass_dict
+
+        return mass_obj
 
 ## Retrieves default values for Behroozi et al. (2013)
 def _retrieve_Behroozi_default_dict():
