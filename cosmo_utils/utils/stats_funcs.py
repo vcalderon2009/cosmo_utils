48ef438a958a382ff2a83dc6fb607a7a658f9de3
Victor Calderon <victor.calderon90@gmail.com>
2019-08-13 12:23:02 -0500

    Added modified function of 'Stats_one_arr', i.e. 'Stats_one_arr_mod'

--- cosmo_utils/utils/stats_funcs.py
+++ cosmo_utils/utils/stats_funcs.py
@@ -13,7 +13,8 @@ __all__        = [  "myceil",
                     "myfloor",
                     "Bins_array_create",
                     "sigma_calcs",
-                    "Stats_one_arr"]
+                    "Stats_one_arr",
+                    "Stats_one_arr_mod"]
 """
 Set of statistical functions
 """
@@ -21,6 +22,7 @@ Set of statistical functions
 ## Import modules
 import math
 import numpy as np
+from scipy.stats import median_absolute_deviation as med_abs_dev
 from   cosmo_utils.utils             import file_utils as fd
 from   cosmo_utils.custom_exceptions import LSSUtils_Error
 
@@ -507,5 +509,210 @@ def Stats_one_arr(x, y, base=1., arr_len=0, arr_digit='n',
         # Populating output object
         for jj, key_val in enumerate(return_val):
             return_obj[jj] = xy_dict[key_val]
+
+    return return_obj
+
+## Function that computes statistics on variables `x` and `y`
+def Stats_one_arr_mod(x, y, base=1., arr_len=0, statfunc=np.nanmean,
+    bin_statval='average', failval=np.nan, type_sigma='std',
+    type_shade='std'):
+    """
+    Computes a set of statistic on two variables `x` and `y`. The function
+    will compute the `statfunc` statistics and errors.
+
+    Parameters
+    ------------
+    x, y : `numpy.ndarray`, `list`, shape (N,)
+        Sets of elements for the 1st and 2nd observables.
+
+    base : `float`, optional
+        Bin width in units of `x`. This variable is set to ``1.`` by default.
+
+    arr_len : `int`, optional
+        Minimum number of elements in each bin of `x`. For example,
+        if ``arr_len == 10``, a statistics is computed only if there
+        are a minimum of ``10`` elements in a specified bin of `x`.
+        This variable is set ``0`` by default.
+
+    stat_func : {`numpy.nanmean`, `numpy.nanmedian`} statistical function, optional
+        Numerical function used to calculate on bins of data. By default,
+        this variable is set to `numpy.nanmean`.
+
+    bin_statval : {'average', 'left', 'right', 'center'} `str`, optional
+        Option for where to put the bin value of `x` and `y`.
+        This variable is set to `average` by default.
+
+        Options:
+            - ``average`` : The function returns the mean of `x`.
+            - ``left`` : The function returns the points to the `left` edge of the bin of `x` and `statfunc` of `y`.
+            - ``right`` : The function returns the points to the `right` edge of the bin of `x` and `statfunc` of `y`.
+            - ``center`` : The function returns the point at the `center` of the bin and `statfunc` of `y`.
+
+    failval : `int`, `float`, `NoneType`, or `NaN`, optional
+        This is the value used when no data is available for the bin. This
+        This variable is set to `numpy.nan` by default.
+
+    type_sigma : {`std``, ``med_abs``} `str`, optional
+        Option for calculating either `percentiles`, `standard deviation`,
+        or ``error in the median``. This variable is set to ``std``
+        by default.
+
+        Options : 
+            - ``std`` : Computes the Standard Deviation of bins in `y`
+            - ``med_abs`` : Computes the Absolute median absolute deviation of bins in `y`
+
+    type_shade : {``std``, ``perc``}, optional
+        Option for which type of statistic to use when determining the
+        ranges of errors. This variable is set to ``perc`` by default.
+
+        Options:
+            - ``std`` : Computes the 1-, 2-, and 3-St. Dev from the mean/median
+            - ``perc`` : Computers the 68th, 95th, and 99.7th percentiles about the median
+
+    Returns
+    ------------
+    stats_dict : `dict`
+        Python dictionary with the necessary statistics for `x` and `y`
+        based on the input arguments.
+    """
+    file_msg = fd.Program_Msg(__file__)
+    ## Verifying input values
+    # Array dimensions
+    if not ((len(x) > 0) and (len(y) > 0)):
+        msg = '{0} The arrays `x` ({1}) and `y` ({2}) must have at least one '
+        msg += 'value in the array!'
+        msg = msg.format(file_msg, len(x), len(y))
+        raise LSSUtils_Error(msg)
+    if not ((np.asarray(x).ndim == 1) and (np.asarray(y).ndim == 1)):
+        msg = '{0} `x` ({1}) and `y` ({2}) must have dimensions of `1`!'
+        msg = msg.format(file_msg, np.asarray(x).ndim, np.asarray(y).ndim)
+        raise LSSUtils_Error(msg)
+    # `base` - Type
+    base_type_arr = (float, int)
+    if not (isinstance(base, base_type_arr)):
+        msg = '{0} `base` ({1}) is not a valid input type ({2})!'
+        msg = msg.format(file_msg, type(base), base_type_arr)
+        raise TypeError(msg)
+    # `base` - Value
+    if not (base > 0):
+        msg = '{0} `base` ({1}) must be larger than `0`!'
+        msg = msg.format(file_msg, base)
+        raise ValueError(msg)
+    # `arr_len` - Type
+    arr_len_type_arr = (float, int)
+    if not (isinstance(arr_len, arr_len_type_arr)):
+        msg = '{0} `arr_len` ({1}) is not a valid input type ({2})!'
+        msg = msg.format(file_msg, type(arr_len), arr_len_type_arr)
+        raise TypeError(msg)
+    else:
+        arr_len = int(arr_len)
+    # `arr_len` - Value
+    if not (arr_len >= 0):
+        msg = '{0} `arr_len` ({1}) must be larger than `0`!'
+        msg = msg.format(file_msg, arr_len)
+        raise ValueError(msg)
+    # `bin_statval` - Type
+    bin_statval_type_arr = (str)
+    if not (isinstance(bin_statval, bin_statval_type_arr)):
+        msg = '{0} `bin_statval` ({1}) is not a valid input type ({2})!'
+        msg = msg.format(file_msg, type(bin_statval), bin_statval_type_arr)
+        raise TypeError(msg)
+    # `bin_statval` - Value
+    bin_statval_val_arr = ['average', 'left', 'right', 'center']
+    if not (bin_statval in bin_statval_val_arr):
+        msg = '{0} `bin_statval` ({1}) is not a valid input value ({2})!'
+        msg = msg.format(file_msg, bin_statval, bin_statval_val_arr)
+        raise ValueError(msg)
+    # `type_sigma` - Type
+    type_sigma_type_arr = (str)
+    if not (isinstance(type_sigma, type_sigma_type_arr)):
+        msg = '{0} `type_sigma` ({1}) is not a valid input type ({2})!'
+        msg = msg.format(file_msg, type(type_sigma), type_sigma_type_arr)
+        raise TypeError(msg)
+    # `type_sigma` - Value
+    type_sigma_val_arr = ['std', 'med_abs']
+    if not (type_sigma in type_sigma_val_arr):
+        msg = '{0} `type_sigma` ({1}) is not a valid input value ({2})!'
+        msg = msg.format(file_msg, type_sigma, type_sigma_val_arr)
+        raise ValueError(msg)
+    # `type_shade` - Type
+    type_shade_type_arr = (str)
+    if not (isinstance(type_shade, type_shade_type_arr)):
+        msg = '{0} `type_shade` ({1}) is not a valid input type ({2})!'
+        msg = msg.format(file_msg, type(type_shade), type_shade_type_arr)
+        raise TypeError(msg)
+    # `type_shade` - Value
+    type_shade_val_arr = ['std', 'perc']
+    if not (type_shade in type_shade_val_arr):
+        msg = '{0} `type_shade` ({1}) is not a valid input value ({2})!'
+        msg = msg.format(file_msg, type_shade, type_shade_val_arr)
+        raise ValueError(msg)
+    ##
+    ## Converting to desired types
+    base    = float(base)
+    arr_len = int(arr_len)
+    ## Converting arrays to `numpy` arrays
+    x = np.asarray(x)
+    y = np.asarray(y)
+    # Fixing `arr_len`
+    arr_len = int(arr_len - 1.) if arr_len !=0 else int(arr_len)
+    #
+    # Statistics calculations
+    x_bins_edges = Bins_array_create(x, base=base)
+    x_digits     = np.digitize(x, x_bins_edges) - 1
+    # Tuple for `x_bins`
+    x_bins       = Bins_array_create(x, base=base, return_tuple=True)
+    nbins        = len(x_bins)
+    #
+    # Determining which bins to use. These are the bins that meet the
+    # criteria of `arr_len`
+    x_digits_bins = np.asarray([int(ii) for ii in range(nbins) if
+        (len(x_digits[x_digits == ii]) > arr_len)])
+    # Computing statistics if there are the necessary number of bins
+    if (len(x_digits_bins) > 0):
+        # Extracting `x` values
+        x_mod = np.asarray([x[x_digits == ii] for ii in x_digits_bins])
+        # Extracting `y` values
+        y_mod = np.asarray([y[x_digits == ii] for ii in x_digits_bins])
+        # Bins that meet the `arr_len` criteria
+        x_bins_criteria = x_bins[x_digits_bins]
+        # Statics on `x`
+        if (bin_statval == 'left'):
+            x_stat = x_bins_criteria.T[0]
+        elif (bin_statval == 'right'):
+            x_stat = x_bins_criteria.T[1]
+        elif (bin_statval == 'center'):
+            x_stat = np.nanmean(x_bins_criteria, axis=1)
+        elif (bin_statval == 'average'):
+            x_stat = np.asarray([np.nanmean(xx) for xx in x_mod])
+        ## Statistics on `y`
+        # Main function on `y`
+        y_stat = np.asarray([statfunc(xx) for xx in y_mod])
+        # Percentiles
+        perc_arr_lims = sigma_calcs(y_mod, type_sigma=type_shade)
+        # Error in `y`
+        if (type_sigma == 'std'):
+            y_err = np.asarray([np.nanstd(xx) for xx in y_mod])
+            if (statfunc == np.nanmedian):
+                y_err *= 1.253
+        elif (type_sigma == 'med_abs'):
+            y_err = np.asarray([med_abs_dev(xx) for xx in y_mod])
+    else:
+        x_stat        = np.array([np.nan])
+        y_stat        = np.array([np.nan])
+        y_err         = np.array([np.nan])
+        x_mod         = np.array([np.nan])
+        y_mod         = np.array([np.nan])
+        perc_arr_lims = dict(zip([0,1,2], [np.array([np.nan]),
+                            np.array([np.nan]), np.array([np.nan])]))
+    #
+    # Saving elements to dictionary
+    return_obj = {}
+    return_obj['x_stat'] = x_stat
+    return_obj['y_stat'] = y_stat
+    return_obj['y_err' ] = y_err
+    return_obj['x_mod' ] = x_mod
+    return_obj['y_mod' ] = y_mod
+    return_obj['perc_arr_lims'] = perc_arr_lims
 
     return return_obj
